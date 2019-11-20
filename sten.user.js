e6aeae0e4d34969a873f84fd11998895500d27ea
theizrael <3961456+theizrael@users.noreply.github.com>
2019-11-20 07:58:03 -0700

    1

---
+++ sten.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Stein UserScript
+// @namespace    https://theizrael.com/js/
+// @version      0.7
+// @description  Stein World UserScript!
+// @author       Theizrael
+// @homepage     https://theizrael.com/js/stein.user.js
+// @icon         https://theizrael.com/js/favicons.png
+// @match        https://stein.world/
+// @include      http://stein.world/
+// @grant        unsafeWindow
+// @grant        GM_addStyle
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_xmlhttpRequest
+// @grant        GM_registerMenuCommand
+// @grant        GM_deleteValue
+// @grant        GM_listValues
+// @grant        GM_getResourceText
+// @grant        GM_getResourceURL
+// @grant        GM_log
+// @grant        GM_openInTab
+// @grant        GM_setClipboard
+// @grant        GM_info
+// @grant        GM_getMetadata
+// @run-at       document-start
+// ==/UserScript==
